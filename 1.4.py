8262ccdf39bb117cd87f06c2f360ea71801af098
Ash McCallum <ash.mccallum@outlook.com>
2017-12-04 20:47:26 +0000

    completed dot products and angles plus tests

--- 1.4.py
+++ 1.4.py
@@ -44,6 +44,15 @@ class Vector(object):
         except ZeroDivisionError:
             raise Exception('Cannot normalize zero vector')
 
+    def dot(self, v):
+        return sum([x * y for x, y in zip(self.coordinates, v.coordinates)])
+
+    def angle(self, v):
+        dot = self.dot(v)
+        mags = self.magnitude() * v.magnitude()
+
+        return math.acos(dot / mags)
+
 add_vector = Vector([8.218, -9.341])
 add_vector2 = Vector([-1.129, 2.111])
 print add_vector.add(add_vector2)
@@ -56,14 +65,30 @@ scalar_num = 7.41
 scalar_vector = Vector([1.671, -1.012, -0.318])
 print scalar_vector.scalar(scalar_num)
 
-v = Vector([-0.221, 7.437])
-print v.magnitude()
+w = Vector([-0.221, 7.437])
+print w.magnitude()
 
-v = Vector([8.813, -1.331, -6.247])
-print v.magnitude()
+w = Vector([8.813, -1.331, -6.247])
+print w.magnitude()
 
-v = Vector([5.581, -2.136])
-print v.normalise()
+w = Vector([5.581, -2.136])
+print w.normalise()
 
-v = Vector([1.996, 3.108, -4.554])
-print v.normalise()
+w = Vector([1.996, 3.108, -4.554])
+print w.normalise()
+
+w = Vector([7.887, 4.138])
+y = Vector([-8.802, 6.776])
+print w.dot(y)
+
+w = Vector([-5.955, -4.904, -1.874])
+y = Vector([-4.496, -8.755, 7.103])
+print w.dot(y)
+
+w = Vector([3.183, -7.627])
+y = Vector([-2.668, 5.319])
+print w.angle(y)
+
+w = Vector([7.35, 0.221, 5.188])
+y = Vector([2.751, 8.259, 3.985])
+print w.angle(y) * 57.2958
