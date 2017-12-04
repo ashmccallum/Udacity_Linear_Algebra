101d96f30b5e71a4c635a2a24f0dd647d1b8ef59
Ash McCallum <ash.mccallum@outlook.com>
2017-12-04 13:56:10 +0000

    first checkin from first quiz on basic vector operations

--- 1.4.py
+++ 1.4.py
@@ -1,3 +1,5 @@
+import math
+
 class Vector(object):
     def __init__(self, coordinates):
         try:
@@ -18,43 +20,50 @@ class Vector(object):
     def __eq__(self, v):
         return self.coordinates == v.coordinates
 
+    def add(self, v):
+        output = [x + y for x, y in zip(self.coordinates, v.coordinates)]
+        return output
 
-def add(vector1, vector2):
+    def minus(self, v):
+        output = [x - y for x, y in zip(self.coordinates, v.coordinates)]
+        return output
 
-    output = []
+    def scalar(self, scalar):
+        output = [scalar * x for x in self.coordinates]
+        return Vector(output)
 
-    for x in range(vector1.dimension):
-        output.insert(x, vector1.coordinates[x] + vector2.coordinates[x])
+    def magnitude(self):
+        coords_squared = [x**2 for x in self.coordinates]
+        return math.sqrt(sum(coords_squared))
 
-    return output
+    def normalise(self):
+        try:
+            magnitude = self.magnitude()
+            return self.scalar(1/magnitude)
 
-def minus(vector1, vector2):
-
-    output = []
-
-    for x in range(vector1.dimension):
-        output.insert(x, vector1.coordinates[x] - vector2.coordinates[x])
-
-    return output
-
-def scalar(scalar, vector):
-
-    output = []
-
-    for x in range(vector.dimension):
-        output.insert(x, vector.coordinates[x] * scalar)
-
-    return output
+        except ZeroDivisionError:
+            raise Exception('Cannot normalize zero vector')
 
 add_vector = Vector([8.218, -9.341])
 add_vector2 = Vector([-1.129, 2.111])
+print add_vector.add(add_vector2)
 
 minus_vector = Vector([7.119, 8.215])
 minus_vector2 = Vector([-8.223, 0.878])
+print minus_vector.minus(minus_vector2)
 
 scalar_num = 7.41
 scalar_vector = Vector([1.671, -1.012, -0.318])
+print scalar_vector.scalar(scalar_num)
 
-print add(add_vector, add_vector2)
-print minus(minus_vector, minus_vector2)
-print scalar(scalar_num, scalar_vector)
+v = Vector([-0.221, 7.437])
+print v.magnitude()
+
+v = Vector([8.813, -1.331, -6.247])
+print v.magnitude()
+
+v = Vector([5.581, -2.136])
+print v.normalise()
+
+v = Vector([1.996, 3.108, -4.554])
+print v.normalise()
